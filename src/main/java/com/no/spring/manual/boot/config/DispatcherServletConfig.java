cd8de7c123bd31f6da78d223f0bf5b998fc282de
Santosh Kumar <santosh.kumar09@sap.com>
2021-05-24 14:04:04 +0530

    Import for separate package comp scan

--- src/main/java/com/no/spring/manual/boot/config/DispatcherServletConfig.java
+++ src/main/java/com/no/spring/manual/boot/config/DispatcherServletConfig.java
@@ -1,19 +1,12 @@
 package com.no.spring.manual.boot.config;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
-public class AppConfig {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
+public class DispatcherServletConfig {
+    
     @Bean
     public DispatcherServlet dispatcherServlet() {
         return new DispatcherServlet();
--- src/main/java/com/no/spring/manual/boot/main/NoSpringBootMagicApplication.java
+++ src/main/java/com/no/spring/manual/boot/main/NoSpringBootMagicApplication.java
@@ -4,11 +4,15 @@ package com.no.spring.manual.boot.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.no.spring.manual.boot.config.MvcConfig;
+import com.no.spring.manual.boot.config.TomcatConfig;
 
 @ComponentScan
 @Configuration
-@EnableWebMvc
+@Import({TomcatConfig.class, DispatcherServlet.class, MvcConfig.class})
 public class NoSpringBootMagicApplication {
 
     public static void main(String[] args) {
