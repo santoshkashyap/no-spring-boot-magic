654136e183ef7ce66fc0468501942af9117453b6
Santosh Kumar <santosh.kumar09@sap.com>
2021-05-24 13:38:15 +0530

    Add TomcatServletWebServerFactory

--- src/main/java/com/no/spring/manual/boot/NoSpringBootMagicApplication.java
+++ src/main/java/com/no/spring/manual/boot/NoSpringBootMagicApplication.java
@@ -2,6 +2,8 @@ package com.no.spring.manual.boot;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,11 @@ public class NoSpringBootMagicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NoSpringBootMagicApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
 
