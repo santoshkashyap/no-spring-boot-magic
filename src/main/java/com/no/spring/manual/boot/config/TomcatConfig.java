3fd154c20893894f5d7fb2f2acba5873cd7acccb
Santosh Kumar <santosh.kumar09@sap.com>
2021-05-24 14:48:09 +0530

    Add conditional for tomcat

--- src/main/java/com/no/spring/manual/boot/config/TomcatConfig.java
+++ src/main/java/com/no/spring/manual/boot/config/TomcatConfig.java
@@ -3,13 +3,28 @@ package com.no.spring.manual.boot.config;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @Configuration
+@Conditional(TomcatConfig.OnTomcatCondition.class)
 public class TomcatConfig {
 
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    public class OnTomcatCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return ClassUtils
+                .isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
+        }
     }
 }
