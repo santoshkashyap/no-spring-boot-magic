ca02e2723c0e81f8624b8dae51f8f854fa7f0939
Santosh Kumar <santosh.kumar09@sap.com>
2021-05-24 13:43:27 +0530

    Add rest controller

--- src/main/java/com/no/spring/manual/boot/NoSpringBootMagicApplication.java
+++ src/main/java/com/no/spring/manual/boot/NoSpringBootMagicApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Configuration
+@Controller
 public class NoSpringBootMagicApplication {
 
     public static void main(String[] args) {
@@ -19,5 +22,9 @@ public class NoSpringBootMagicApplication {
         return new TomcatServletWebServerFactory();
     }
 
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
 
 }
